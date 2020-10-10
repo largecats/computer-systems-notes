57d24358dcb134a3b3018475da6398627abff751
xiaolf <xiaolf@seagroup.com>
2020-10-10 15:25:03 +0800

    updated

---
+++ chapter2/2.1.3/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "show_bytes.h"
+
+void test_show_bytes(int val);
+
+int main() {
+    int x = 12345;
+    test_show_bytes(x);
+    return 0;
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+
+    putchar('\n');
+
+    /* practice problem 2.5 */
+    int a = 0x12345678;
+    byte_pointer ap = (byte_pointer) &a;
+    show_bytes(ap, 1);
+    show_bytes(ap, 2);
+    show_bytes(ap, 3);
+}
+
+/*
+$ gcc chapter2/2.1.3/main.c chapter2/2.1.3/show_bytes.c -o chapter2/2.1.3/show_bytes
+$ chapter2/2.1.3/show_bytes
+39 30 00 00
+00 e4 40 46
+f8 76 e8 c4 ff 7f 00 00
+
+78
+78 56
+78 56 34
+*/
+
+/*
+decimal: 12345
+hexadecimal: 0x3039
+*/
