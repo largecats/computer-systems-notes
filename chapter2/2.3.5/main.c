82eff05e2f61090f25cda00de71fc6ff1ddbba8d
xiaolf <xiaolf@seagroup.com>
2020-12-06 15:24:13 +0800

    updated

---
+++ chapter2/2.3.5/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include "../../helper_functions.h"
+
+int tmult_ok(int x, int y);
+
+int main() {
+    int x = pow(2, 32-1)-1;
+    int y = x;
+    printf("Multiply without overflow? %d\n", tmult_ok(x, y));
+}
+
+int tmult_ok(int x, int y) {
+    int64_t p = (int64_t) x*y;
+    print_long_variable(p);
+    return p == (int) p;
+}
+
+/*
+$ gcc chapter2/2.3.5/main.c helper_functions.c -o chapter2/2.3.5/output.o
+$ chapter2/2.3.5/output.o
+p = 4611686014132420609
+Multiply without overflow? 0
+*/
